e9853a3eca1c707aa28ef9c22de711053b859a70
Jesus Alejandro Molina Llarena <llarenalejo@gmail.com>
2024-09-25 14:03:00 -0500

    added start new menu

--- isbnScrapper.py
+++ isbnScrapper.py
@@ -5,27 +5,26 @@ from TituloClass import *
 from SendTitleToApi import *
 from sendEmail import *
 from readPages import leer_paginas
+import cutie
 
 #go to the isbn website and look for information of the titles of certain editorial given a editorial id
 print("inicia aplicación")
 
-try:
-    if len(sys.argv)>1:
-        first_argument=sys.argv[1]
         
-    else:
-        print("args vacio")
-except Exception  as e:
-    print("Error:{e}")
 
+options=['Look for latests ISBNs','Add title by ID']
+print(f"select a function to execute:")
+selected_option=cutie.select(options)
 
-if first_argument=="1":
+if selected_option==0:
+    print("Looking for latests ISBNs")
     for i in range(1,10,1):
         print(i)
         numero_pag=str(i)
-        leer_paginas(Editoriales.Panini,numero_pag)
-        
-if first_argument=="2":
+        leer_paginas(Editoriales.Panini,numero_pag)    
+
+elif selected_option==1:
+    print("Adding title by ID")
     idTituloBuscar=input("id del titulo a agregar:")
     print("id a buscar: ",idTituloBuscar)
     try:
@@ -33,8 +32,7 @@ if first_argument=="2":
         SendToApi(nuevo_titulo)
 
     except Exception as e:
-        print("error en id "+str(idTituloBuscar)+" "+e.__str__)                
-        
-    
+        print("error en id "+str(idTituloBuscar)+" "+e.__str__)
+
 print("finaliza aplicación")
 
